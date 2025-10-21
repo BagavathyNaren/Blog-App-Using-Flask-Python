8f6bca8def829cffcf8dfc4cd0caea33e40208d1
BagavathyNaren <bagvannan@gmail.com>
2025-10-21 17:27:26 +0530

    Add requests import to main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 import os
+import requests
 import smtplib
 
 '''
